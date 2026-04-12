27d5df9bbfcf2f32e4f0d268e4c6bb12c1c51fdc
Oleksii Budnik <budnikoleksii@gmail.com>
2026-04-12 09:53:26 +0300

    fix: lint error

--- apps/money-tracker/src/proxy.ts
+++ apps/money-tracker/src/proxy.ts
@@ -1,10 +1,9 @@
-import type { NextRequest } from 'next/server';
+import type { NextRequest, NextResponse } from 'next/server';
 
 import { MiddlewareTokenProvider } from '@track-my-life/next-shared/src/api/client/token/middleware-token-provider';
 import { routing } from '@track-my-life/next-shared/src/i18n/navigation/navigation';
 import { verifyToken } from '@track-my-life/shared/src/utils/jwt';
 import createIntlMiddleware from 'next-intl/middleware';
-import type { NextResponse } from 'next/server';
 
 import { applySecurityHeaders, generateNonce } from '@/utils/middleware/csp';
 import { checkIsPublicPath } from '@/utils/middleware/path';
